c85fced495c939799ab6c6ccfd8f8f209236ed1b
sadrisso <sadrisso2000@gmail.com>
2024-02-14 11:16:48 +0600

    start project practice

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+<link rel="stylesheet" href="style.css">
+<title>Welcome to laravel</title>
+</head>
+<body class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <ul class="nav ">
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/')}}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/')}}">View</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/')}}">Trash</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <h1 class="text-center mt-5">Hi, I am learning laravel</h1>
+    </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
